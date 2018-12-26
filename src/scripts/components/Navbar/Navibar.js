db67a02f64892a9568b5a1eabcc6be9c34bf1c6b
Phil Lucks <philip.lucks@snapengage.com>
2018-12-26 15:00:01 -0500

    navbar

--- src/scripts/components/Navbar/Navibar.js
+++ src/scripts/components/Navbar/Navibar.js
@@ -15,6 +15,47 @@ class Navibar extends Component {
     isOpen: false,
     active: null
   };
+
+  navbarColorEl = React.createRef();
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = e => {
+    e.preventDefault();
+    this.checkForElementInView(e);
+  };
+
+  checkForElementInView = e => {
+    let active;
+    for (let ref in this.props.refs) {
+      let { top, bottom } = this.props.refs[ref].getBoundingClientRect();
+      if (top < window.innerHeight && bottom >= 0) {
+        switch (ref) {
+          case "aboutRef":
+            active = "about";
+            break;
+          case "projectsRef":
+            active = "projects";
+            break;
+          case "contactRef":
+            active = "contact";
+            break;
+          default:
+            break;
+        }
+      } else if (window.scrollY < 60) {
+        active = null;
+      }
+    }
+    this.setState({ active });
+  };
+
   toggle = e => {
     e.preventDefault();
     this.setState({
@@ -26,12 +67,14 @@ class Navibar extends Component {
     if (!e) return false;
     e.preventDefault();
 
+    if (!scrollTo) {
+      scrollTo = document.body;
+    }
+
     scrollTo.scrollIntoView({
       behavior: "smooth",
       block: "start"
     });
-
-    this.setState({ active: e.target.name });
   };
 
   render() {
@@ -39,10 +82,22 @@ class Navibar extends Component {
 
     return (
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">phlucks</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
+        <span
+          id="nav-color-bar"
+          ref={this.navbarColorEl}
+          style={{ width: `${this.state.offsetLeft}%` }}
+        />
+        <NavbarBrand
+          onClick={e => {
+            this.handleClick(e);
+          }}
+          className="z-50"
+        >
+          phlucks
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggle} className="z-50" />
         <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="ml-auto" navbar>
+          <Nav className="ml-auto z-50" navbar>
             <NavItem>
               <NavLink
                 className={this.state.active === "about" ? "active" : ""}
